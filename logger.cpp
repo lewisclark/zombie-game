dc636c7f9dbcf69c4faa5796d4fa57dbd4b559bb
= <=>
2019-01-06 16:02:30 +0000

    Add logger class

--- logger.cpp
+++ logger.cpp
@@ -1,3 +1,12 @@
 #include "logger.h"
 
+game::Logger::Logger(const std::string& filename) {
+	m_ofstream = std::ofstream(filename, std::ios_base::app);
+}
+
+void game::Logger::LogString(const std::string& s) {
+	m_ofstream << s;
+	m_ofstream.flush();
+}
+
 std::unique_ptr<game::Logger> game::logger = nullptr;
--- logger.h
+++ logger.h
@@ -2,12 +2,24 @@
 #define LOGGER_H
 
 #include <memory>
+#include <fstream>
+
+#include "fmtlib/format.h"
 
 namespace game {
 	class Logger {
 		public:
+		Logger(const std::string& filename);
+
+		void LogString(const std::string& s);
+
+		template <typename... Args>
+		void LogFormat(const std::string& fmt, const Args&... args) {
+			LogString(fmt::format(fmt.c_str(), args...));
+		}
 
 		private:
+		std::ofstream m_ofstream;
 	};
 
 	extern std::unique_ptr<Logger> logger;
