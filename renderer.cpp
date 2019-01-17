f27d560fbea7ac50e9d317043348ead2758723ec
= <=>
2019-01-17 21:42:39 +0000

    Implement Render methods

--- renderer.cpp
+++ renderer.cpp
@@ -19,3 +19,19 @@ game::Size game::Renderer::GetCanvasSize() {
 	
 	return Size(w, h);
 }
+
+bool game::Renderer::Clear() {
+	return SDL_RenderClear(m_sdlrenderer) ? false : true;
+}
+
+void game::Renderer::Present() {
+	SDL_RenderPresent(m_sdlrenderer);
+}
+
+bool game::Renderer::SetDrawColor(const Color& col) {
+	return SDL_SetRenderDrawColor(m_sdlrenderer, col.GetR(), col.GetG(), col.GetB(), col.GetA()) ? false : true;
+}
+
+bool game::Renderer::SetDrawColor(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a) {
+	return SDL_SetRenderDrawColor(m_sdlrenderer, r, g, b, a) ? false : true;
+}
--- renderer.h
+++ renderer.h
@@ -3,7 +3,10 @@
 
 #include <SDL2/SDL.h>
 
+#include <cinttypes>
+
 #include "position.h"
+#include "color.h"
 
 namespace game {
 	class Renderer {
@@ -13,6 +16,11 @@ namespace game {
 
 		void Destroy();
 		Size GetCanvasSize();
+
+		bool Clear();
+		void Present();
+		bool SetDrawColor(const Color& col);
+		bool SetDrawColor(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a);
 
 		private:
 		SDL_Renderer* m_sdlrenderer = nullptr;
