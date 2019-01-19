34d9cfbbf88389c75b07a69298c9456e762b79fe
= <=>
2019-01-19 13:45:53 +0000

    Getter/Setters for Character variables

--- character.cpp
+++ character.cpp
@@ -11,3 +11,27 @@ void game::Character::Think(const InputManager* const inputmgr) {
 bool game::Character::IsValid() const {
 	return Entity::IsValid();
 }
+
+const int& game::Character::GetStrength() const {
+	return m_strength;
+}
+
+const int& game::Character::GetDefence() const {
+	return m_defence;
+}
+
+const int& game::Character::GetSpeed() const {
+	return m_speed;
+}
+
+void game::Character::SetStrength(const int& strength) {
+	m_strength = strength;
+}
+
+void game::Character::SetDefence(const int& defence) {
+	m_defence = defence;
+}
+
+void game::Character::SetSpeed(const int& speed){ 
+	m_speed = speed;
+}
--- character.h
+++ character.h
@@ -10,10 +10,18 @@ namespace game {
 		virtual void Think(const InputManager* const inputmgr);
 		virtual bool IsValid() const;
 
+		const int& GetStrength() const;
+		const int& GetDefence() const;
+		const int& GetSpeed() const;
+
+		void SetStrength(const int& strength);
+		void SetDefence(const int& defence);
+		void SetSpeed(const int& speed);
+
 		private:
-		int strength;
-		int defence;
-		int speed;
+		int m_strength = 1;
+		int m_defence = 1;
+		int m_speed = 1;
 	};
 }
 
