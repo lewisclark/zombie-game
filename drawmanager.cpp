2d3f6cb134f4488ccd098ccbb96f94183cd45c33
= <=>
2019-01-19 13:20:29 +0000

    Present() last

--- drawmanager.cpp
+++ drawmanager.cpp
@@ -8,9 +8,10 @@ game::DrawManager::DrawManager(Renderer* ren) :
 void game::DrawManager::DrawWorld(World* world) {
 	m_renderer->SetDrawColor(42, 42, 42, 255);
 	m_renderer->Clear();
-	m_renderer->Present();
 
 	DrawEntities(world->GetEntities());
+
+	m_renderer->Present();
 }
 
 void game::DrawManager::DrawEntities(const std::array<Entity*, game::MAX_ENTITIES>& entities) {
